8dc537e53b2a8d0e32578610a39d770815a92918
inesrodriguez <irpdevelop@gmail.com>
2019-02-19 14:11:59 -0300

    receiving parameter from android app

--- js/player-caf.js
+++ js/player-caf.js
@@ -57,18 +57,18 @@ class Player {
         this.playerManager.setMediaPlaybackInfoHandler((loadRequest, pc) => {
             const customData = loadRequest.media.customData;
             const licenseUrl = customData['hss_license_url'] || null;
-            //const watermarkText = customData['hss_watermark_text'] || null;
+            const watermarkText = customData['hss_watermark_text'] || null;
             // const licenseCustomData = customData['hss_license_custom_data'] || null;
             pc.licenseUrl = licenseUrl;
             // pc.licenseCustomData = licenseCustomData;
             pc.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
-            //setWatermarkText(watermarkText);
+            setWatermarkText(watermarkText);
             return pc;
         });
     }
     
     setWatermarkText(watermarkText) {
-        document.getElementById('watermark').innerHTML = watermarkText;
+        document.getElementById('WatermarkHss').innerHTML = watermarkText;
     }
 
     setMessageInterceptors() {
